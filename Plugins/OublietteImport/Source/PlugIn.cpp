60ee531f18539927587f2f4d96f835ea093a816d
eyebex <eyebex@03a14c8b-b77e-4111-8d5a-c8012f88454e>
2007-05-04 19:45:43 +0000

    Minor changes to justify a new version for KeePass 1.07 ;-)
    git-svn-id: svn://svn.code.sf.net/p/keepass/code/trunk@71 03a14c8b-b77e-4111-8d5a-c8012f88454e

--- Plugins/OublietteImport/Source/PlugIn.cpp
+++ Plugins/OublietteImport/Source/PlugIn.cpp
@@ -32,8 +32,8 @@
 // The first three digits of the plug-in version number match the KeePass
 // version number for which the plug-in was compiled. The last digit marks
 // the release number of the plug-in for this particular KeePass version.
-#define OUB_IMP_FORAPP  0x01000601
-#define OUB_IMP_VERSION 0x01000601
+#define OUB_IMP_FORAPP  0x01000701
+#define OUB_IMP_VERSION 0x01000701
 
 #define OUB_IMP_RELEASE 3
 
