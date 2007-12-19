87e6089fa97892dd6885a6a1e3f1fce463b4d0c0
eyebex <eyebex@03a14c8b-b77e-4111-8d5a-c8012f88454e>
2007-12-19 08:06:44 +0000

    Added usage note.
    git-svn-id: svn://svn.code.sf.net/p/keepass/code/trunk@86 03a14c8b-b77e-4111-8d5a-c8012f88454e

--- Plugins/OublietteImport/Source/PlugIn.cpp
+++ Plugins/OublietteImport/Source/PlugIn.cpp
@@ -120,7 +120,9 @@ KP_EXP BOOL KP_API KeePluginCall(DWORD dwCode,LPARAM lParamW,LPARAM lParamL) {
                 _countof(buffer),
                 "A plug-in to import Oubliette OUB files (see http://oubliette.sf.net/).\n"
                 "This is release %u of the version compiled for KeePass %u.%u%u.\n"
-                "Written by Sebastian Schuberth <sschuberth@gmail.com>.",
+                "Written by Sebastian Schuberth <sschuberth@gmail.com>.\n\n"
+                "Usage note: You need to first create or open a KeePass database file\n"
+                "to import to before the plug-in's menu items get enabled."
                 OUB_IMP_RELEASE,
                 VERSION_MASK(OUB_IMP_FORAPP,24),VERSION_MASK(OUB_IMP_FORAPP,16),VERSION_MASK(OUB_IMP_FORAPP,8)
             );
