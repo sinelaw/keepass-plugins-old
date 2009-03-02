6e2cb63085d0c2e939a4092a7e4dff251300d824
eyebex <eyebex@03a14c8b-b77e-4111-8d5a-c8012f88454e>
2009-03-02 00:12:27 +0000

    Copied files from non-COM plugin project.
    git-svn-id: svn://svn.code.sf.net/p/keepass/code/trunk@118 03a14c8b-b77e-4111-8d5a-c8012f88454e

---
+++ Plugins/OublietteImportCOM/Source/Resource.h
@@ -0,0 +1,16 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by OublietteImport.rc
+
+#define IDD_OUB_PWD_DIALOG              102
+#define IDC_OUB_PWD_EDIT                1005
+
+// Next default values for new objects:
+#ifdef APSTUDIO_INVOKED
+    #ifndef APSTUDIO_READONLY_SYMBOLS
+        #define _APS_NEXT_RESOURCE_VALUE        104
+        #define _APS_NEXT_COMMAND_VALUE         40001
+        #define _APS_NEXT_CONTROL_VALUE         1006
+        #define _APS_NEXT_SYMED_VALUE           101
+    #endif
+#endif
